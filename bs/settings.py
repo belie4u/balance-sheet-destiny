30374709ea14ea184a6f49f151238fb8ddd9a1c8
Byiringiro Elie <byiringiro4u@gmail.com>
2025-04-18 14:48:49 +0300

    Update settings.py
    added alowed host

--- bs/settings.py
+++ bs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://belie.pythonanywhere.com','127.0.0.1']
 
 # Application definition
 
